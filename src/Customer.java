a0b98086dad33e37e312d1d05841f6b283dcef31
Amit Joshi <amit.a.joshi@gmail.com>
2019-08-01 20:34:07 +0530

    Initial commit

---
+++ src/Customer.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+    private String name;
+    private List<Rental> rentals = new ArrayList<>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental rental) {
+
+        rentals.add(rental);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String statement() {
+        int frequentRenterPoints = 0;
+        String result = "Rental Record for " + getName() + "\n";
+        for (Rental rental : rentals) {
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
+        }
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+        return result;
+    }
+
+    private double getTotalCharge() {
+        return rentals.stream().mapToDouble(Rental::getCharge).sum();
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        return rentals.stream().mapToInt(Rental::getFrequentRenterPoints).sum();
+    }
+
+
+}
---
+++ test/CustomerTest.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void statementKids() {
+    Customer customer=new Customer("little kid");
+    Movie movie=new Movie("Dumbo",2);
+    customer.addRental(new Rental(movie,5));
+    String actual=customer.statement();
+    String expected="Rental Record for little kid\n" +
+            "\tDumbo\t4.5\n" +
+            "Amount owed is 4.5\n" +
+            "You earned 1 frequent renter points";
+    assertEquals(actual,expected);
+
+    }
+
+    @Test
+    void statementRegular() {
+        Customer customer=new Customer("uncle pancho");
+        Movie movie=new Movie("Rambo First Blood",0);
+        customer.addRental(new Rental(movie,7));
+        String actual=customer.statement();
+        String expected="Rental Record for uncle pancho\n" +
+                "\tRambo First Blood\t9.5\n" +
+                "Amount owed is 9.5\n" +
+                "You earned 1 frequent renter points";
+        assertEquals(actual,expected);
+
+    }
+
+    @Test
+    void statementNewRelease() {
+        Customer customer=new Customer("new bossy");
+        Movie movie=new Movie("Anabelle",1);
+        customer.addRental(new Rental(movie,5));
+        String actual=customer.statement();
+        String expected="Rental Record for new bossy\n" +
+                "\tAnabelle\t15.0\n" +
+                "Amount owed is 15.0\n" +
+                "You earned 2 frequent renter points";
+        assertEquals(actual,expected);
+
+    }
+
+
+}
